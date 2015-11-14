e8b0a31cd5af96bc50ff1bd6dfa719f2b0519252
Paul Bryan <pabryan@gmail.com>
2015-11-14 14:55:19 +0100

    Updated publishing file to my generic system.

--- publish.el
+++ publish.el
@@ -1,32 +1,5 @@
-(setq pb/conformal_mcf_solitons-org-publish-project-alist
-      '(("conformal_mcf_solitons-org"
-	:base-directory "~/working/academic/research/conformal_mcf_solitons"
-	:publishing-directory "~/publish/conformal_mcf_solitons"
-	:base-extension "org"
-	:recursive t
-	:publishing-function org-latex-publish-to-latex
-	:export-with-tags nil
-	:headline-levels 10
-	:with-toc nil
-	:section-numbers t
-;	:sub-superscript nil
-	:todo-keywords nil
-	:author nil
-	:creator-info nil
-	:table-auto-headline t
-	:style-include-default t
-	:timestamp f
-	:exclude-tags ("noexport")
-	:auto-preamble t)
+(setq pb/org-current-project "conformal_mcf_solitons")
+(setq pb/org-src-path  (concat "~/working/academic/research/" pb/org-current-project))
+(setq pb/org-publish-path (concat "~/publish/academic/research/" pb/org-current-project))
 
-	("conformal_mcf_solitons-static"
-	 :base-directory "~/working/academic/research/conformal_mcf_solitons"
-	 :publishing-directory "~/publish/conformal_mcf_solitons"
-	 :recursive t
-	 :base-extension "tex\\|png\\|jpg\\|gif\\|pdf"
-	 :publishing-function org-publish-attachment
-	 )
-
-	("conformal_mcf_solitons" :components ("conformal_mcf_solitons-org" "conformal_mcf_solitons-static"))))
-
-(setq org-publish-project-alist pb/conformal_mcf_solitons-org-publish-project-alist)
+(setq org-publish-project-alist (pb/create-org-publish-alist pb/org-current-project pb/org-src-path pb/org-publish-path))
